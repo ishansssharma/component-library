75bf27fd2c8ddb5eafbeb219c8ee26558a93d1de
ishansssharma <143998034+ishansssharma@users.noreply.github.com>
2023-11-08 16:25:22 +0100

    creating more elements

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ overviewButtons.forEach(button => {
 
 })
 
+
 function displayComponent(event) {
 	const button = event.currentTarget;
 	const componentToDisplay = button.dataset.component;
@@ -30,3 +31,75 @@ function displayComponent(event) {
 
 	})
 }
+
+
+const accordionContainer = document.querySelector(".accordion__expanded");
+
+const accordionExpanded = document.querySelector(".accordion__expanded--info--visible");
+
+accordionContainer.addEventListener("click", function () {
+
+	accordionExpanded.classList.toggle("accordion__expanded--info--visible");
+});
+
+const clickedMessage = document.querySelector(".clicked__button");
+const activeButton = document.querySelector(".click__me--active");
+
+activeButton.addEventListener("click", () => {
+	clickedMessage.classList.add("clicked__button--visible");
+
+	setTimeout(() => {
+		clickedMessage.classList.remove("clicked__button--visible");
+	}, 1200);
+});
+
+const toastGreen = document.querySelector(".toast__normal");
+const buttonToastGreen = document.querySelector(".toast-button__green");
+
+
+
+
+buttonToastGreen.addEventListener("click", () => {
+	toastGreen.classList.add("toast__normal--visible");
+	setTimeout(() => {
+		toastGreen.classList.remove("toast__normal--visible")
+	}, 2000);
+});
+
+const toastOrange = document.querySelector(".toast__advarsel");
+const buttonToastOrange = document.querySelector(".toast-button__orange");
+
+buttonToastOrange.addEventListener("click", () => {
+	toastOrange.classList.add("toast__advarsel--visible");
+	setTimeout(() => {
+		toastOrange.classList.remove("toast__advarsel--visible")
+	}, 2000);
+});
+
+const toastRed = document.querySelector(".toast__error");
+const buttonToastRed = document.querySelector(".toast-button__red");
+
+buttonToastRed.addEventListener("click", () => {
+	toastRed.classList.add("toast__error--visible");
+	setTimeout(() => {
+		toastRed.classList.remove("toast__error--visible")
+	}, 2000);
+});
+
+const createToast = () => {
+
+}
+
+
+
+
+
+
+const animalChips = document.querySelectorAll(".choicechip-animal");
+
+animalChips.forEach(animalChip => {
+	animalChip.addEventListener("click", () => {
+		animalChip.classList.toggle("choicechip-animal--click");
+	})
+
+});
